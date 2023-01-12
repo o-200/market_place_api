849d62fcc1a45794ea3babcda0f96f82761baba5
Alexander Abramov <omgout200@gmail.com>
2023-01-12 16:22:39 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespaces :api, defaults: { format: :json } do
-    namespaces :v1 do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
 
     end
   end
