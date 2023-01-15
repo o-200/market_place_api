151215d0effffce7cd7896a2b9181f189f38ab94
Alexander Abramov <omgout200@gmail.com>
2023-01-15 15:07:27 +0700

    add tests for token controller and fix that

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,7 +2,7 @@ module Authenticable
   def current_user
     return @current_user if @current_user
 
-    header.request.headers['Authorization']
+    header = request.headers['Authorization']
     return nil if header.nil?
 
     decoded = JsonWebToken.decode(header)
